fb9ac5c3fe1cf23e5b821e2754cf2e70f2a4e3e5
多田秀人 <shoot@Shootie-Air.local>
2020-09-04 13:10:50 +0900

    added common soundplayer method.

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -16,52 +16,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    // mp3 of cymbal
+    // path for mp3 files
     let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
-    
-    // create player instance of cymbal
-    var cymbalPlayer = AVAudioPlayer()
-
-    @IBAction func cymbal(_ sender: Any) {
-        do {
-            // set mp3 file to cymbal player
-            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
-            // playing mp3
-            cymbalPlayer.play()
-        } catch {
-            print("シンバルで、エラーが発生しました！")
-        }
-    }
-    
-    // mp3 of guitar
     let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
-    var guitarPlayer = AVAudioPlayer()
-    @IBAction func guitar(_ sender: Any) {
-        do {
-            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            guitarPlayer.play()
-        } catch {
-            print("ギターで、エラーが発生しました！")
-        }
-    }
-    
-    // mp3 of bgm
     let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    // player instances
+    var cymbalPlayer = AVAudioPlayer()
+    var guitarPlayer = AVAudioPlayer()
     var backmusicPlayer = AVAudioPlayer()
     
+    @IBAction func cymbal(_ sender: Any) {
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
+    }
+    
+    @IBAction func guitar(_ sender: Any) {
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+    }
+    
     @IBAction func play(_ sender: Any) {
-        do {
-            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-            backmusicPlayer.numberOfLoops = -1
-            backmusicPlayer.play()
-        } catch {
-            print("エラーが発生しました！")
-        }
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
     }
     
     @IBAction func stop(_ sender: Any) {
         // stop bgm
         backmusicPlayer.stop()
+    }
+    
+    // common player
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました！")
+        }
     }
 }
 
